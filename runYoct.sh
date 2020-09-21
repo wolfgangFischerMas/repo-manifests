92290b194d40f9812b864e0bf859896233e76a5c
WolfgangFischerMas <w.fischer@mas-software.de>
2020-09-21 15:10:02 +0200

    yocto scripts added

---
+++ runYoct.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+
+doAll()             {
+    instlDockr
+    preprDryoct
+    runDryoct
+    instRepstrTask3
+    bitbkImg
+}
+help()              {
+doAll
+echo "USAGE $0
+instlDockr
+preprDryoct
+runDryoct
+instRepstrTask3
+bitbkImg
+"
+}
+instlDockr()        {
+    sudo apt-get update && sudo apt-get upgrade
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
+    sudo usermod -aG docker [user_name]
+    docker version
+    docker info
+    docker run hello-world
+}
+preprDryoct()       {
+    mkdir /public/Work
+    cd /public/Work
+    git clone https://github.com/bstubert/dr-yocto.git
+    cd dr-yocto
+    git checkout ubuntu-18.04
+    ./build.sh 18.04 ./18.04
+}
+runDryoct()         {
+    cd /public/Work
+    ./dr-yocto/run-shell.sh 18.04
+}
+instRepstrTask3()   {
+	mkdir /public/Work/yoctTaskS
+	rm -rf /public/Work/yoctTaskS/build-rpi3-aufgabe3/
+	rm -rf /public/Work/yoctTaskS/sources/
+	ls -la /public/Work/yoctTaskS/
+	cd /public/Work/yoctTaskS
+	repo init -u https://github.com/wolfgangFischerMas/repo-manifests.git -m repo-yocto-aufgabe3.xml 
+        repo sync
+}
+bitbkImg()          {
+	export TEMPLATECONF=/public/Work/yoctTaskS/sources/meta-aufgabe3/custom
+	source ./sources/poky/oe-init-build-env build-rpi3-aufgabe3
+	cd /public/Work/yoctTaskS/build-rpi3-aufgabe3
+	bitbake aufgabe4-image
+}
+
+COMND="help"; if [ "$1" != "" ] ; then COMND="$1"; fi; $COMND "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9"
---
+++ runYocto.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+# repo-manifests
+
+## installation
+### 1. install docker
+
+	sudo apt-get update && sudo apt-get upgrade
+	curl -fsSL https://get.docker.com -o get-docker.sh
+	sudo sh get-docker.sh
+	sudo usermod -aG docker [user_name]
+	docker version
+	docker info
+	docker run hello-world				
+
+### 2. prepare dr. yocto docker feature
+	mkdir /public/Work
+	cd /public/Work
+	git clone https://github.com/bstubert/dr-yocto.git
+	cd dr-yocto
+	git checkout ubuntu-18.04
+	./build.sh 18.04 ./18.04
+
+### 3. run dr. yocto docker feature interactive
+	./dr-yocto/run-shell.sh 18.04
+	
+#### 3.1 check out / sync the yocto project repo
+	mkdir /public/Work/yoctTaskS
+	rm -rf /public/Work/yoctTaskS/build-rpi3-aufgabe3/
+	rm -rf /public/Work/yoctTaskS/sources/
+	ls -la /public/Work/yoctTaskS/
+	cd /public/Work/yoctTaskS
+	repo init -u https://github.com/wolfgangFischerMas/repo-manifests.git -m repo-yocto-aufgabe3.xml 
+	repo sync 
+
+#### 3.2 bitbake the required image
+	export TEMPLATECONF=/public/Work/yoctTaskS/sources/meta-aufgabe3/custom
+	source ./sources/poky/oe-init-build-env build-rpi3-aufgabe3
+	cd /public/Work/yoctTaskS/build-rpi3-aufgabe3
+	bitbake aufgabe4-image
